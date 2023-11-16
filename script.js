ddb76a3bcfd81133e8bb7129e649df5be7afbe2e
Azamat2004 <kokosherovazamat04@gmail.com>
2023-11-16 16:52:08 +0600

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('button').addEventListener('click', function() {
+
+    var textOneValue = document.getElementById('one').value;
+    var textTwoValue = document.getElementById('two').value;
+
+    document.getElementById('one').value = textTwoValue;
+    document.getElementById('two').value = textOneValue;
+});
